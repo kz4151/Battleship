f755948e36a243b3d3b49efa4da0ec38c9d873d3
Kevin Zhang <kz4151@vt.edu>
2017-10-12 18:47:57 -0400

    Bug fixes

--- src/Game.java
+++ src/Game.java
@@ -17,7 +17,7 @@ public class Game {
     public void initShips(boolean player1) {
         for (int i = 0; i < 7; i++) {
             setShipPositions(i, player1);
-            if(player1) {
+            if (player1) {
                 printP1Grid();
             }
             else {
@@ -55,11 +55,21 @@ public class Game {
                         "Input V for vertical ship placement. Input H for horizontal ship placement");
                 orient = sc.next();
                 if (orient.equals("V")) {
-                    resultCode = p1.setShip(x, y, true, type);
+                    if (player1) {
+                        resultCode = p1.setShip(x, y, true, type);
+                    }
+                    else {
+                        resultCode = p2.setShip(x, y, true, type);
+                    }
                     properOrient = true;
                 }
                 else if (orient.equals("H")) {
-                    resultCode = p2.setShip(x, y, false, type);
+                    if (player1) {
+                        resultCode = p1.setShip(x, y, false, type);
+                    }
+                    else {
+                        resultCode = p2.setShip(x, y, false, type);
+                    }
                     properOrient = true;
                 }
                 else {
@@ -82,32 +92,32 @@ public class Game {
         }
     }
 
-private String setShipName(int type) {
-    String shipName = " ";
-    switch (type) {
-    case 0:
-        shipName = "Air Carrier";
-        break;
-    case 1:
-        shipName = "Battleship";
-        break;
-    case 2:
-        shipName = "Cruiseship";
-        break;
-    case 3:
-        shipName = "Destroyer 1";
-        break;
-    case 4:
-        shipName = "Destroyer 2";
-        break;
-    case 5:
-        shipName = "Submarine 1";
-        break;
-    case 6:
-        shipName = "Submarine 2";
-        break;
+    private String setShipName(int type) {
+        String shipName = " ";
+        switch (type) {
+        case 0:
+            shipName = "Air Carrier";
+            break;
+        case 1:
+            shipName = "Battleship";
+            break;
+        case 2:
+            shipName = "Cruiseship";
+            break;
+        case 3:
+            shipName = "Destroyer 1";
+            break;
+        case 4:
+            shipName = "Destroyer 2";
+            break;
+        case 5:
+            shipName = "Submarine 1";
+            break;
+        case 6:
+            shipName = "Submarine 2";
+            break;
+        }
+        return shipName;
     }
-    return shipName;
-}
 
 }
