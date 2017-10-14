7812636d9e14c3fb15bc731ec00a92f3335de4d6
Kevin Zhang <kz4151@vt.edu>
2017-10-13 22:41:52 -0400

    Bug fixes

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,43 +1,44 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Battleship {
     public static void main(String[] args) {
         // This is the main file for the program.
         Game battle = new Game();
-//        battle.initShips(true);
-//        battle.initShips(false);
-        boolean gameOver = false; 
+        // battle.initShips(true);
+        // battle.initShips(false);
+        boolean gameOver = false;
         boolean legalCoor;
         int turn = 0;
         Scanner sc = new Scanner(System.in);
         int x;
         int y;
-        while(!gameOver) {
+        while (!gameOver) {
             turn++;
             legalCoor = false;
-            if(turn % 2 == 0) {
-                // Player 2's turn 
+            if (turn % 2 == 0) {
+                // Player 2's turn
                 System.out.println("Player 2's Turn");
-                while(!legalCoor) {
+                while (!legalCoor) {
                     System.out.println("Enter a X-coordinate between 0-9.");
                     x = sc.nextInt();
                     System.out.println("Enter a Y-coordinate between 0-9.");
                     y = sc.nextInt();
-                    legalCoor = checkUserInput(x,y);
+                    legalCoor = checkUserInput(x, y);
                 }
                 battle.printP1GameGrid();
                 System.out.println("Press any key to end your turn");
                 sc.next();
             }
             else {
-                // Player 1's turn 
+                // Player 1's turn
                 System.out.println("Player 1's Turn");
-                while(!legalCoor) {
+                while (!legalCoor) {
                     System.out.println("Enter a X-coordinate between 0-9.");
                     x = sc.nextInt();
                     System.out.println("Enter a Y-coordinate between 0-9.");
                     y = sc.nextInt();
-                    legalCoor = checkUserInput(x,y);
+                    legalCoor = checkUserInput(x, y);
                 }
                 battle.printP2GameGrid();
                 System.out.println("Press any key to end your turn");
@@ -45,9 +46,16 @@ public class Battleship {
             }
         }
     }
-    
+
     public static boolean checkUserInput(int x, int y) {
-        if(x > 10 && x < 0 || y > 10 && y < 0) {
+        if ((x > 10 || x < 0)) {
+            System.out.println(
+                    "Please input X-Coordinates that are between 0 to 9");
+            return false;
+        }
+        else if (y > 10 || y < 0) {
+            System.out.println(
+                    "Please input Y-Coordinates that are between 0 to 9");
             return false;
         }
         else {
